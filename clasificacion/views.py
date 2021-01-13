ae81e72954780c098bdd793e9b104091b54e9045
Stefanny Aguirre <sbaguirr@espol.edu.ec>
2021-01-13 18:49:10 -0500

    se habilitó método post para el formulario, se visualizan los campos elegidos en la sección de resultados

--- clasificacion/views.py
+++ clasificacion/views.py
@@ -5,8 +5,36 @@ from django.shortcuts import render
 def index(request):
     return render(request, "clasificacion/index.html")
 
+
 def hashtag(request):
-    return render(request, "clasificacion/hashtag.html")    
+    if request.method == "POST":
+        hashtag = request.POST["hashtag"]
+        cantidad = request.POST["cantidad"]
+        mesDesde = request.POST["mesDesde"]
+        diaDesde = request.POST["diaDesde"]
+        anioDesde = request.POST["anioDesde"]
+        mesHasta = request.POST["mesHasta"]
+        diaHasta = request.POST["diaHasta"]
+        anioHasta = request.POST["anioHasta"]
+        pedidosAyuda = []
+        quejas = []
+        ofertas = []
+        pedidosAyuda.append(hashtag)
+        pedidosAyuda.append(cantidad)
+        pedidosAyuda.append(mesDesde)
+        quejas.append(diaDesde)
+        quejas.append(anioDesde)
+        ofertas.append(mesHasta)
+        ofertas.append(diaHasta)
+        ofertas.append(anioHasta)
+        return render(request, "clasificacion/hashtag.html", {
+            "pedidosAyuda": pedidosAyuda,
+            "quejas": quejas,
+            "ofertas": ofertas
+        })
+    else:
+        return render(request, "clasificacion/hashtag.html")
+
 
 def tweet(request):
-    return render(request, "clasificacion/tweet.html")    
+    return render(request, "clasificacion/tweet.html")
